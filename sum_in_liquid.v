f98ae93f8fe141ae2a285d16fdc7c8654c0840ef
Hamad alghanim <hamadyalghanim@gmail.com>
2024-11-04 20:43:06 -0700

    fix issue with Program Syntax

--- sum_in_liquid.v
+++ sum_in_liquid.v
@@ -22,16 +22,10 @@ Next Obligation.
     apply le_0_n.
   - (* v >= k *)
     lia.
-Next Obligation.
-  split.
-  - (* s + k >= 0 *)
-    apply le_0_n.
-  - (* s + k >= S k' *)
-    lia.
 Defined.
 
 
-Compute (proj1_sig (sum_checked 3)).
+Compute (proj1_sig (sum_checked 5)).
 Compute (proj2_sig (sum_checked 3)).
 
 Example test_sum_1: proj1_sig (sum_checked 3) = 6.
